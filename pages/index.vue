7025a1fd952701d850892e91f1913338bdd6a656
ApriCityAohan <1448366829@qq.com>
2024-12-30 12:01:24 +0800

    feat: 特性(pages): 🚀 主页导航bolg详情页面,和详情页渲染
    feat：index 页面跳转详情链接
    
    feat：详情页面渲染,Head头
    
    feat：minddleware 全局拦截,动态Head头(未知情况待修复)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <markdown />
+  <NuxtLink to="/p/10"> 跳转博客页面 </NuxtLink>
 </template>
 
 <script setup lang="ts"></script>
---
+++ pages/p/[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ $route.params.id }} - 博客页面</div>
+  <markdown />
+</template>
+
+<script setup lang="ts">
+import { useCopyCode } from "markdown-it-copy-code";
+const route = useRoute();
+
+const { id } = route.params;
+
+useHead({
+  title: `Post ${id}`,
+});
+
+onMounted(() => {
+  useCopyCode();
+});
+</script>
+
+<style lang="scss" scoped></style>
