c8cb24588ec2b3c74f2c063cb7df27b08627ea73
ApriCityAohan <1448366829@qq.com>
2024-12-30 18:23:47 +0800

    feat: 特性(api/type): 🚀 新增Issue类型接口,以及Issue依赖接口,具体接口查看详情
    feat:Issue接口
    Label接口
    Creator(创建者)接口
    Milestone(里程碑)接口
    Reaction(反响)接口

--- api/type.d.ts
+++ api/type.d.ts
@@ -1,3 +1,4 @@
+/** Readme 文章数据 */
 export interface Readme {
   name: string;
   path: string;
@@ -15,4 +16,111 @@ export interface Readme {
     git: string;
     html: string;
   };
+}
+
+/** Issue 数据 */
+export interface Issue {
+  url: string;
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+  html_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  user: Creator;
+  labels: Label[];
+  state: string;
+  locked: boolean;
+  assignee: string;
+  assignees: any[];
+  milestone: Milestone;
+  comments: number;
+  created_at: string;
+  updated_at: string;
+  closed_at: string;
+  author_association: string;
+  active_lock_reason: string;
+  body: string;
+  reactions: Reaction;
+  timeline_url: string;
+  performed_via_github_app: string;
+  state_reason: string;
+}
+
+/**
+ * Label 数据
+ */
+export interface Label {
+  id: number;
+  node_id: string;
+  url: string;
+  name: string;
+  color: string;
+  default?: boolean;
+  description?: string;
+}
+
+/**
+ * 创建者数据
+ */
+interface Creator {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+/**
+ * 里程碑数据
+ */
+export interface Milestone {
+  url: string;
+  html_url: string;
+  labels_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  description: string;
+  creator: Creator;
+  open_issues: number;
+  closed_issues: number;
+  state: string;
+  created_at: string;
+  updated_at: string;
+  due_on: string;
+  closed_at: string;
+}
+
+/**
+ * 反应数据
+ */
+interface Reaction {
+  url: string;
+  total_count: number;
+  "+1": number;
+  "-1": number;
+  laugh: number;
+  hooray: number;
+  confused: number;
+  heart: number;
+  rocket: number;
+  eyes: number;
 }
