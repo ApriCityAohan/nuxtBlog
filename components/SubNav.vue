a00ef119eeae07f4bda49ee6e5eb8bc9e9343022
ApriCityAohan <1448366829@qq.com>
2025-01-03 16:39:01 +0800

    feat: 特性: 🚀 blog列表页面基础布局实现
    feat:blog主页面基础结构编写
    feat:新增subnav组件

---
+++ components/SubNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <ul class="flex gap-3 mb-3 text-2xl md:text-4xl mt-7">
+      <li class="cursor-pointer transition-opacity" @click="$router.push('/blog')">
+        <span>All</span>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
--- pages/blog/[[catelog]].vue
+++ pages/blog/[[catelog]].vue
@@ -1,7 +1,52 @@
 <template>
-  <div></div>
+  <div class="pattern-cross-dots-md bg-gray-50 text-gray-400 px-12">
+    <div class="zw max-w-3xl mx-auto text-black">
+      <SubNav />
+      <Divider>
+        <template #content>
+          <div class="mx-4">A total of <b>17</b> articles</div>
+        </template>
+      </Divider>
+      <ul id="blog-list">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import gsap from "gsap";
 
-<style></style>
+let gsapCtx: gsap.Context;
+
+onMounted(() => {
+  gsapCtx = gsap.context(() => {
+    gsap.from("#blog-list li", {
+      duration: 0.5,
+      y: 10,
+      opacity: 0,
+      stagger: 0.1,
+      ease: "power1.in",
+    });
+  });
+});
+
+onUnmounted(() => {
+  gsapCtx.revert();
+});
+</script>
+
+<style scoped lang="scss">
+.zw {
+  padding-top: var(--ah-header-height);
+}
+</style>
