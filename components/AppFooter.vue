af0eeba5f563e221c6210d676590fd635708186d
ApriCityAohan <1448366829@qq.com>
2025-01-03 10:11:02 +0800

    feat: 特性: 🚀 footer基础布局实现

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,23 @@
 <template>
-  <div></div>
+  <footer class="py-5 pattern-cross-dots-md bg-gray-50 text-gray-400">
+    <div
+      class="flex gap-2 flex-col items-center justify-center text-black opacity-70 text-sm md:flex-row"
+    >
+      <NuxtLink
+        to="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+        target="__blank"
+        class="underline"
+        rel="noopener noreferrer"
+      >
+        CC BY-NC-SA 4.0
+      </NuxtLink>
+      <span>@{{ VITE_OWNER }}</span>
+    </div>
+  </footer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { VITE_OWNER } = import.meta.env;
+</script>
 
 <style lang="scss" scoped></style>
