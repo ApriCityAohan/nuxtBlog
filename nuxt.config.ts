bfaebb580bb86f87d56a3f62868794d419cc8cca
ApriCityAohan <1448366829@qq.com>
2025-01-03 14:50:52 +0800

    feat: 添加全局样式文件 global.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/icon", "@nuxt/image"],
-  css: ["@/assets/styles/markdown.scss", "pattern.css/dist/pattern.min.css"],
+  app: {
+    head: {
+      noscript: [{ innerHTML: "This website requires JavaScript." }],
+      bodyAttrs: {
+        class: "font-sans",
+      },
+    },
+  },
+  css: [
+    "@/assets/styles/markdown.scss",
+    "@/assets/styles/global.css",
+    "pattern.css/dist/pattern.min.css",
+  ],
   colorMode: {
     storage: "localStorage",
   },
