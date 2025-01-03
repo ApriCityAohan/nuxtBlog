901f0a7bfb6af8f1f3e67e9d58cf5efd8c8a85f2
ApriCityAohan <1448366829@qq.com>
2025-01-03 15:24:27 +0800

    feat: 特性: 🚀 更新了Divider分割线自定义插槽

--- components/Divider.vue
+++ components/Divider.vue
@@ -2,6 +2,7 @@
   <div class="flex items-center text-lg md:text-xl font-bold m-8">
     <div class="flex-auto border-b border-dashed border-b-slate-500"></div>
     <div class="mx-4" v-if="title">{{ title }}</div>
+    <slot name="content"></slot>
     <div class="flex-auto border-b border-dashed border-b-slate-500"></div>
   </div>
 </template>
