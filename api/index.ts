2b82cd775ef521aa4818cf3912785eca83a46e14
ApriCityAohan <1448366829@qq.com>
2025-01-06 15:48:46 +0800

    feat: 特性: 🚀 新增获取里程碑API
    feat:新增获取里程碑
    fix:修复环境变量命名错误

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,38 @@
-import type { Readme, IssueResult, Issue } from "./type";
+import type { Readme, IssueResult, Issue, Milestone } from "./type";
 import type { IssueQuery } from "./query";
 
 // 环境变量
-const { VITE_OWNER, VITE_BLOG_REPO } = import.meta.env;
+const { VITE_OWNER, VITE_BLOGS_REPO } = import.meta.env;
 
 /* 获取README -- 主要在个人介绍页面（About） */
 export const getReadme = () => {
   return useGithubFetch<Readme>(
-    `https://api.github.com/repos/${VITE_OWNER}/${VITE_BLOG_REPO}/readme`,
+    `https://api.github.com/repos/${VITE_OWNER}/${VITE_BLOGS_REPO}/readme`,
   );
 };
 
 /* 获取Issus列表 */
 export const getIssues = () => {
   return useGithubFetch<Issue[]>(
-    `https://api.github.com/repos/${VITE_OWNER}/${VITE_BLOG_REPO}/issues`,
+    `https://api.github.com/repos/${VITE_OWNER}/${VITE_BLOGS_REPO}/issues`,
   );
 };
 
 /* 搜索Issue */
 export const searchIssues = (q: string, opt?: IssueQuery) => {
   // 拼接查询字符串
-  let query = `?q=${q}+repo:${VITE_OWNER}/${VITE_BLOG_REPO}+is:issue+state:open`;
+  let query = `?q=${q}+repo:${VITE_OWNER}/${VITE_BLOGS_REPO}+is:issue+state:open`;
   // 如果有查询条件
   if (opt) {
     query += buildQueryStr(opt);
   }
 
   return useGithubFetch<IssueResult>(`https://api.github.com/search/issues${query}`);
+};
+
+/* 里程碑分类 */
+export const getMilestones = () => {
+  return useGithubFetch<Milestone[]>(
+    `https://api.github.com/repos/${VITE_OWNER}/${VITE_BLOGS_REPO}/milestones`,
+  );
 };
