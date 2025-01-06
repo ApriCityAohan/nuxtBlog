5c4b8c289a6b00bb54fc7c176f7c897bc8877e6e
ApriCityAohan <1448366829@qq.com>
2025-01-06 09:49:26 +0800

    feat: 特性: 🚀 文章列表页基础布局及功能
    feat:文章列表页面布局及数据渲染
    feat:文章列表组件
    fix:修复页面命名错误

---
+++ components/IssueCell.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="overflow-auto">
+    <NuxtLink
+      :to="`/p/${issue.number}`"
+      class="w-full mb-6 flex flex-col transition-opacity opacity-70 hover:opacity-100 md:flex-row md:items-center md:gap-2"
+    >
+      <span class="text-sm op-70 font-mono">{{ formatDate(issue.created_at) }}</span>
+      <span class="hidden md:inline">——</span>
+      <span class="text-lg">{{ issue.title }}</span>
+      <div></div>
+    </NuxtLink>
+  </li>
+</template>
+
+<script lang="ts" setup>
+const { issue } = defineProps(["issue"]);
+</script>
+
+<style></style>
---
+++ pages/blog/[[catalog]].vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="px-12">
+    <div class="zw max-w-3xl mx-auto text-black">
+      <SubNav />
+      <Divider>
+        <template #content>
+          <div class="mx-4">
+            A total of <b>{{ searchResult?.total_count }}</b> articles
+          </div>
+        </template>
+      </Divider>
+      <ul>
+        <template v-for="issue in issuesList" :key="issue.id">
+          <div v-if="issue.isMarked" class="relative pointer-events-none select-none h-20">
+            <span class="year-stroke font-bold font-mono text-transparent opacity-15 absolute">{{
+              issue.year
+            }}</span>
+          </div>
+          <IssueCell v-else :issue="issue" />
+        </template>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { searchIssues } from "@/api";
+import gsap from "gsap";
+
+const route = useRoute();
+// 编目参数
+const catalog = route.params.catalog as string;
+
+// 搜索文章结果
+const searchResult = ref();
+// 文章列表
+const issuesList = ref();
+
+/**
+ *  搜索文章
+ *
+ * @param q 搜索关键字
+ */
+async function handleSearch(q: string) {
+  // 如果有编目参数，则搜索该编目下的文章
+  const { data } = catalog ? await searchIssues(q, { milestone: catalog }) : await searchIssues(q);
+  // 搜索结果（需要展示数量用）
+  searchResult.value = data.value;
+  // 处理插入年份后的列表
+  issuesList.value = insertYearToPosts(data.value?.items);
+}
+
+await handleSearch("");
+
+let gsapCtx: gsap.Context;
+
+onMounted(() => {
+  gsapCtx = gsap.context(() => {
+    // gsap.from("#blog-list li", {
+    //   duration: 0.5,
+    //   y: 10,
+    //   opacity: 0,
+    //   stagger: 0.1,
+    //   ease: "power1.in",
+    // });
+  });
+});
+
+onUnmounted(() => {
+  gsapCtx.revert();
+});
+</script>
+
+<style scoped lang="scss">
+.zw {
+  padding-top: var(--ah-header-height);
+}
+
+.year-stroke {
+  top: -0.2em;
+  left: -0.3em;
+  font-size: 8em;
+  -webkit-text-stroke: 2px rgba(170, 170, 170, 1);
+}
+</style>
