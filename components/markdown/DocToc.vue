cd3964f65982302fa68708ecef019bb77002775b
ApriCityAohan <1448366829@qq.com>
2025-01-07 17:40:26 +0800

    fix: 修复: 🧩 修复了文章页响应式问题

--- components/markdown/DocToc.vue
+++ components/markdown/DocToc.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute top-0 left-full h-full w-[20em] pl-[2em] opacity-30 hover:opacity-100 transition-opacity"
+    class="absolute top-0 left-full h-full w-[20em] pl-[2em] opacity-30 hover:opacity-100 transition-opacity hidden md:block"
   >
     <div
       class="sticky top-32 w-[25em] max-h-[70vh] overflow-y-auto overscroll-contain"
