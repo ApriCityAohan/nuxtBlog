68c9535ff4f39f2d522255fd0b99f8e9dea87b9c
ApriCityAohan <1448366829@qq.com>
2025-01-07 15:49:54 +0800

    feat: 特性: 🚀 注册markdownit锚点和目录组件

--- components/markdown/markdow-it.ts
+++ components/markdown/markdow-it.ts
@@ -2,10 +2,20 @@ import markdownIt from "markdown-it";
 import PluginHighligh from "markdown-it-highlightjs";
 // @ts-ignore
 import PluginTaskCheck from "markdown-it-task-checkbox";
+import PluginAnchor from "markdown-it-anchor";
+import PluginToc from "markdown-it-toc-done-right";
 import PluginCopy from "markdown-it-copy-code";
 import "highlight.js/styles/atom-one-dark.css";
 import "markdown-it-copy-code/styles/base.css";
 import "markdown-it-copy-code/styles/large.css";
+
+// Toc类型
+interface TocType {
+  html?: string;
+  ast?: object;
+}
+
+export const toc = ref<TocType>({});
 
 export const md = new markdownIt({
   html: true,
@@ -13,6 +23,18 @@ export const md = new markdownIt({
   typographer: true,
   xhtmlOut: true,
 })
+  .use(PluginAnchor, {
+    level: [2, 3, 4],
+    permalink: true,
+    permalinkBefore: true,
+    permalinkSymbol: "#",
+  })
+  .use(PluginToc, {
+    listType: "ul",
+    callback(html: any, ast: any) {
+      toc.value = { html, ast };
+    },
+  })
   .use(PluginTaskCheck)
   .use(PluginHighligh)
   .use(PluginCopy);
