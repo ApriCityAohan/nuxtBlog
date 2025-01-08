2bf6101bb1e7098bc461a33ef29199decbaac28e
ApriCityAohan <1448366829@qq.com>
2025-01-08 17:51:07 +0800

    feat: 特性: 🚀 更改了组件样式

--- components/IssueCell.vue
+++ components/IssueCell.vue
@@ -2,11 +2,14 @@
   <li class="overflow-auto dark:text-white">
     <NuxtLink
       :to="`/p/${issue.number}`"
-      class="w-full mb-6 flex flex-col transition-opacity opacity-70 hover:opacity-100 md:flex-row md:items-center md:gap-2"
+      class="w-full mb-6 flex flex-col transition-opacity opacity-70 hover:opacity-100 md:flex-row md:items-center md:gap-1.5"
     >
-      <span class="text-sm op-70 font-mono">{{ formatDate(issue.created_at) }}</span>
-      <span class="hidden md:inline">——</span>
-      <span class="text-lg">{{ issue.title }}</span>
+      <span class="opacity-70 font-mono flex items-center gap-1.5">
+        {{ formatDate(issue.created_at) }} <span class="hidden md:inline-block">·</span>
+      </span>
+      <span class="text-lg">
+        {{ issue.title }}
+      </span>
       <div></div>
     </NuxtLink>
   </li>
