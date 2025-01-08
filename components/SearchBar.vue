e0f37e11f9595bdf55e01404c6bcddade381bd42
ApriCityAohan <1448366829@qq.com>
2025-01-08 17:53:53 +0800

    feat: 特性: 🚀 搜索页面完善完毕

---
+++ components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex gap-5">
+    <input
+      v-model="q"
+      type="text"
+      class="search-input"
+      placeholder="Search post title..."
+      @keyup.enter="search"
+    />
+    <button class="search-button" @click="search">
+      <Icon name="mingcute:search-ai-line"></Icon>
+      Search
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const q = ref(""); // search 参数
+
+const emit = defineEmits<{ search: [q: string] }>();
+
+function search() {
+  emit("search", q.value);
+}
+</script>
+
+<style lang="scss" scoped>
+.search-input {
+  @apply flex-1 px-4 py-1.5 outline-none rounded-md ring-1 ring-current text-gray-800 dark:text-gray-200 hover:text-gray-400 focus:text-gray-400 bg-transparent transition-colors;
+}
+.search-button {
+  @apply flex items-center gap-1 px-4 rounded-md  ring-1 ring-current text-gray-800 dark:text-gray-200 hover:text-gray-400 bg-transparent transition-colors;
+}
+</style>
--- pages/search.vue
+++ pages/search.vue
@@ -1,9 +1,69 @@
 <template>
-  <div>1111111111</div>
+  <section class="max-w-3xl mx-auto px-4 py-8 md:px-0">
+    <h1 class="text-xl md:text-3xl mb-[2em] font-bold">Search</h1>
+
+    <!-- 搜索框 -->
+    <SearchBar @search="handleSearch" />
+
+    <!-- 标签 -->
+    <ul class="flex gap-4 flex-wrap my-[2em]" id="labels">
+      <li
+        v-for="label in labels"
+        :key="label.id"
+        class="rounded-md px-2 py-1 cursor-pointer"
+        :style="{ backgroundColor: `#${label.color}cc` }"
+        @click="handleLabels(label.name)"
+      >
+        #{{ label.name }}
+      </li>
+    </ul>
+
+    <!-- Issue文章列表 -->
+    <ul id="issues">
+      <IssueCell v-for="issue in issues" :key="issue.id" :issue="issue" />
+    </ul>
+  </section>
 </template>
 
 <script lang="ts" setup>
-import { getIssues } from "@/api";
+import gsap from "gsap";
+import { getIssues, getLabels, searchIssues } from "@/api";
+
+// 获取所有 labels
+const { data: labels } = await getLabels();
+
+const issues = ref();
+
+// dom 更新后执行动画
+function animate() {
+  nextTick(() => {
+    gsap.from("#issues li", { opacity: 0, y: 20, duration: 0.7, stagger: 0.1 });
+  });
+}
+
+// 获取所有 issues
+async function handleLabels(label: string) {
+  // 重置 issues
+  issues.value = [];
+  // 获取所有 issues
+  const { data } = await getIssues({ labels: label });
+  // 设置 issues
+  issues.value = data.value;
+
+  if (issues.value.length) animate();
+}
+
+// 搜索
+async function handleSearch(q: string) {
+  const { data } = await searchIssues(q);
+  issues.value = data.value?.items;
+
+  if (issues.value.length) animate();
+}
+
+onMounted(() => {
+  gsap.from("#labels li", { opacity: 0, y: 20, duration: 0.8, stagger: 0.1 });
+});
 </script>
 
 <style></style>
