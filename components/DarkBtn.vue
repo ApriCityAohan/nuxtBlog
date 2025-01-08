e96ae49710272a632e8d4e5d55a827bdb0b4fb25
ApriCityAohan <1448366829@qq.com>
2025-01-09 00:36:18 +0800

    fix: 修复: 🧩 最终解决方案无视水和问题,部署问题无碍

--- components/DarkBtn.vue
+++ components/DarkBtn.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="cursor-pointer opacity-70 transition-opacity hover:opacity-100" @click="handleDark">
-    <ClientOnly>
-      <Icon v-show="$colorMode.value === 'dark'" name="mingcute:moon-stars-line" size="24"></Icon>
-      <Icon v-show="$colorMode.value === 'light'" name="mingcute:sun-fill" size="24"></Icon>
-    </ClientOnly>
+    <!-- 
+      会发生水和问题，但使用ColorScheme和ClientOnly
+      解决了水和问题，
+      可是不能解决在vercel部署后图标请求丢失问题；
+    -->
+    <Icon v-show="$colorMode.value === 'dark'" name="mingcute:moon-stars-line" size="24"></Icon>
+    <Icon v-show="$colorMode.value === 'light'" name="mingcute:sun-fill" size="24"></Icon>
   </div>
 </template>
 
